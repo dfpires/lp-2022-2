ddad1a22733bde756de468ad1f05211ff791783a
Daniel Pires <dfpires@gmail.com>
2022-08-16 09:15:13 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -1,1 +1,12 @@
-imc.js
+function calcular() {
+    // recupera o valor da altura digitado pelo usuário na caixa de texto
+    let altura = document.getElementById("altura").value
+    // recupera o valor do peso digitado pelo usuário na caixa de texto
+    let peso = document.getElementById("peso").value
+    // calcula o imc
+    let imc = peso / (altura * altura)
+    // mostra o resultado
+    //alert(`O valor do imc é de ${imc.toFixed(2)}`) // template string
+    document.getElementById("imc").innerHTML = 
+                `O valor do imc é de ${imc.toFixed(2)}`
+}
